aaea993b69b4f4ec19c8ba0a1d4c11308c4d2d64
Ilya Kuznetsov <ilkztsff@gmail.com>
2025-02-11 23:51:21 +0300

    add class Logger

--- src/infrastructure/loggers.py
+++ src/infrastructure/loggers.py
@@ -1,7 +1,24 @@
-from logging import getLogger
+from datetime import datetime
+from enum import Enum
+from logging import Logger as _Logger
 
 
-app = getLogger('app')
-domain = getLogger('domain')
-infrastructure = getLogger('infrastructure')
-presentation = getLogger('presentation')
+class LogLevel(int, Enum):
+    DEBUG = 1
+    INFO = 2
+    WARNING = 3
+    ERROR = 4
+    CRITICAL = 5
+
+
+class Logger(_Logger):
+    def log(self, msg: str, level: LogLevel, *args, **kwargs) -> None:  # type: ignore[override, no-untyped-def]
+        dt = datetime.now()
+        text = f'[{dt.year}-{dt.month}-{dt.day} {dt.hour}:{dt.minute}:{dt.second}] #{level.name}    {__name__}: {self.name} - {msg}'
+        super(Logger, self).log(level.value, text, *args, **kwargs)
+
+
+app = Logger('app')
+domain = Logger('domain')
+infrastructure = Logger('infrastructure')
+presentation = Logger('presentation')
