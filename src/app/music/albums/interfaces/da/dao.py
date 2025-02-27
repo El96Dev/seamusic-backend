1a175494c6d306845961ef2b16f536fd16fd1d02
Ilya Kuznetsov <ilkztsff@gmail.com>
2025-02-27 19:27:45 +0300

    transfer bin specification to infrastructure

--- src/app/music/albums/interfaces/da/dao.py
+++ src/app/music/albums/interfaces/da/dao.py
@@ -11,13 +11,13 @@ from src.app.music.tracks.models import Track
 from src.app.social.tags.models import Tag
 from src.domain.music.albums.interfaces.da.dao import DAO
 from src.infrastructure.loggers import app as logger
-from src.infrastructure.postgres import PostgresSessionMixin, engine
+from src.infrastructure.postgres import PostgresSessionMixin
 
 
 @dataclass
 class PostgresDAOImplementation(PostgresSessionMixin, DAO):
     def __init__(self) -> None:
-        super(PostgresDAOImplementation, self).__init__(bind=engine, table=Album)
+        super(PostgresDAOImplementation, self).__init__(table=Album)
         self.table = Album
 
     async def get_album_by_id(self, album_id: int) -> Album | None:
@@ -27,35 +27,35 @@ class PostgresDAOImplementation(PostgresSessionMixin, DAO):
 
     async def get_album_existance_by_title(self, artist_id: int, title: str) -> bool:
         logger.info("get_album_existance_by_title DAO request")
-        return bool(await self.run(select(Album).filter(Album.title == title, Album.artists.any() == artist_id), "scalar"))
+        return bool(await self.run(select(Album).group_by(Album.id).filter(Album.title == title, Album.artists.any() == artist_id), "scalar"))
 
     async def get_album_existance_by_id(self, album_id: int) -> bool:
         logger.info("get_album_existance_by_id DAO request")
-        return bool(await self.run(select(Album).filter(Album.id == album_id), "scalar"))
+        return bool(await self.run(select(Album).group_by(Album.id).filter(Album.id == album_id), "scalar"))
 
     async def get_popular_albums(self, start: int, size: int) -> list[Album]:  # type: ignore[override]
         logger.info("get_popular_albums DAO request")
-        return await self.run(select(Album).order_by(func.count(Album.viewers_ids)).offset(start - 1).limit(size), "scalars")
+        return await self.run(select(Album).group_by(Album.id).order_by(func.count(Album.viewers_ids)).offset(start - 1).limit(size), "scalars")
 
     async def count_artist_albums(self, artist_id: int) -> int:
         logger.info("count_artist_albums DAO request")
-        return await self.run(select(func.count(ArtistProfile.albums)).filter(ArtistProfile.id == artist_id), "scalar")
+        return await self.run(select(func.count(ArtistProfile.albums)).group_by(Album.id).filter(ArtistProfile.id == artist_id), "scalar")
 
     async def count_albums(self) -> int:
         logger.info("count_albums DAO request")
-        return await self.run(select(func.count(Album.id)), "scalar")
+        return await self.run(select(func.count(Album.id).group_by(Album.id)), "scalar")
 
     async def get_artist_id_by_user_id(self, user_id: int) -> int | None:
         logger.info("get_artist_id_by_user_id DAO request")
-        return await self.run(select(User.artist_id).filter(User.id == user_id), "scalar")
+        return await self.run(select(User.artist_id).group_by(User.id).filter(User.id == user_id), "scalar")
 
     async def get_artist_existance_by_id(self, artist_id: int) -> bool:
         logger.info("get_artist_existance_by_id DAO request")
-        return bool(await self.run(select(ArtistProfile.id).filter(ArtistProfile.id == artist_id), "scalar"))
+        return bool(await self.run(select(ArtistProfile.id).group_by(ArtistProfile.id).filter(ArtistProfile.id == artist_id), "scalar"))
 
     async def get_artist_albums(self, artist_id: int) -> list[Album]:  # type: ignore[override]
         logger.info("get_artist_albums DAO request")
-        return list(await self.run(select(ArtistProfile.albums).filter(ArtistProfile.id == artist_id).order_by(Album.created_at.desc()), "scalars"))
+        return list(await self.run(select(ArtistProfile.albums).group_by(ArtistProfile.id).filter(ArtistProfile.id == artist_id).order_by(Album.created_at.desc()), "scalars"))
 
     async def create_album(
         self,
@@ -85,7 +85,7 @@ class PostgresDAOImplementation(PostgresSessionMixin, DAO):
             picture_url=picture_url,
             description=description,
         )
-        await self.write(album)
+        await self.create(album)
         return album.id
 
     async def update_album(
@@ -126,7 +126,7 @@ class PostgresDAOImplementation(PostgresSessionMixin, DAO):
 
     async def delete_album(self, album_id: int) -> None:
         logger.info("delete_album DAO request")
-        await self.remove(obj_id=album_id)
+        await self.delete_(obj_id=album_id)
 
     async def __aenter__(self) -> Self:
         return self
