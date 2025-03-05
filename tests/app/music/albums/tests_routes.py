3d08b95655bfff2453cdadfa86014b30b2b27abb
Ilya Kuznetsov <ilkztsff@gmail.com>
2025-03-05 20:21:35 +0300

    fix tests

--- tests/app/music/albums/tests_routes.py
+++ tests/app/music/albums/tests_routes.py
@@ -1,213 +1,250 @@
-from typing import TypedDict
+from datetime import date, datetime
+from tempfile import SpooledTemporaryFile
+from typing import Callable, Literal
 
-from fastapi.testclient import TestClient
-from fastapi import status
 import pytest
+from fastapi import status, UploadFile
+from fastapi.testclient import TestClient
 
-from src.domain.music.albums.core.exceptions import AlbumNotFoundError
+from src.app.music.albums.api.routes import Router, get_router
+from src.presentation.music.albums.schemas import (
+    SCreateAlbumRequest,
+    SCreateAlbumResponse,
+    SAlbumResponse,
+    SUpdateAlbumRequest,
+    SUpdateAlbumResponse,
+    SPopularAlbumsResponse,
+    SUpdateAlbumCoverRequest,
+)
+from tests.app.music.albums.fixtures import AlbumTestModel, album  # noqa: F401
 
 
-class AlbumTestModel(TypedDict):
-    """
-        TypedDict class for check types
-    """
-    album_id: int | None
-    album_title: str
-    album_description: str
-    album_tags: list[str]
-    artists_ids: list[int]
-    tracks_ids: list[int]
+class TestRouter:
+    @pytest.fixture(scope="function")
+    def router_factory(self) -> Callable[[], Router]:
+        return get_router
 
+    @pytest.fixture(scope="function")
+    def album_id(self, album: AlbumTestModel) -> int:
+        return album["id"]
 
-class TestAlbumRoutes:
-    """
-        Fixtures
-    """
+    @pytest.fixture(scope="function")
+    def album_title(self, album: AlbumTestModel) -> str:
+        return album["title"]
 
-    @pytest.fixture(scope='session')
-    async def fixture_user_id(self) -> int:
+    @pytest.fixture(scope="function")
+    def album_type(self, album: AlbumTestModel) -> Literal["album", "single"]:
+        return album["type"]
+
+    @pytest.fixture(scope="function")
+    def album_created_at(self, album: AlbumTestModel) -> date:
+        return album["created_at"]
+
+    @pytest.fixture(scope="function")
+    def album_updated_at(self, album: AlbumTestModel) -> datetime:
+        return album["updated_at"]
+
+    @pytest.fixture(scope="function")
+    def album_viewers_ids(self, album: AlbumTestModel) -> list[int]:
+        return album["viewers_ids"]
+
+    @pytest.fixture(scope="function")
+    def album_likers_ids(self, album: AlbumTestModel) -> list[int]:
+        return album["likers_ids"]
+
+    @pytest.fixture(scope="function")
+    def album_artists_ids(self, album: AlbumTestModel) -> list[int]:
+        return album["artists_ids"]
+
+    @pytest.fixture(scope="function")
+    def album_tracks_ids(self, album: AlbumTestModel) -> list[int]:
+        return album["tracks_ids"]
+
+    @pytest.fixture(scope="function")
+    def album_tags(self, album: AlbumTestModel) -> list[str]:
+        return album["tags"]
+
+    @pytest.fixture(scope="function")
+    def artist_id(self) -> int:
         return 1
 
-    @pytest.fixture(scope='session')
-    async def fixture_artists_id(self) -> int:
+    @pytest.fixture(scope="function")
+    def album_picture_url(self, album: AlbumTestModel) -> str | None:
+        return album["picture_url"]
+
+    @pytest.fixture(scope="function")
+    def album_description(self, album: AlbumTestModel) -> str | None:
+        return album["description"]
+
+    @pytest.fixture(scope="function")
+    def start(self) -> int:
         return 1
 
-    @pytest.fixture(scope='session')
-    def fixture_album_title(self) -> str:
-        return 'title'
-
-    @pytest.fixture(scope='session')
-    def fixture_album_description(self) -> str:
-        return 'description'
-
-    @pytest.fixture(scope='session')
-    def fixture_album_tags(self) -> list[str]:
-        return ['one tag', 'two tag']
-
-    @pytest.fixture(scope='session')
-    def fixture_artists_ids(self) -> list[int]:
-        return []
-
-    @pytest.fixture(scope='session')
-    def fixture_tracks_ids(self) -> list[int]:
-        return []
-
-    @pytest.fixture(scope='session')
-    def fixture_artist_id(self) -> int:
+    @pytest.fixture(scope="function")
+    def size(self) -> int:
         return 1
 
-    @pytest.fixture(scope='session')
-    def fixture_image_bytes(self) -> bytes:
-        f = open('album_photo.jpg', 'rb')
-        return f.read()
+    @pytest.fixture(scope="function")
+    def user_id(self) -> int:
+        return 1
 
-    @pytest.fixture(scope='session')
-    def fixture_picture_url(self) -> str:
-        return 'https://vk.com/image.png'
+    def test_create_album(  # TODO: add parametrized statuses after auth module is done
+        self,
+        client: TestClient,
+        album: AlbumTestModel,
+    ) -> None:
+        request_json = SCreateAlbumRequest(
+            title=album["title"],
+            description=album["description"],
+            tags=album["tags"],
+        ).__dict__
+        response = client.post(url="/albums/", json=request_json)
+        assert response.status_code in [
+            status.HTTP_201_CREATED,
+            status.HTTP_409_CONFLICT,
+            status.HTTP_403_FORBIDDEN,
+            status.HTTP_401_UNAUTHORIZED,
+        ]
+        assert isinstance(response.json(), dict)
+        if response.status_code == status.HTTP_201_CREATED:
+            model = SCreateAlbumResponse(**response.json())
+            album["id"] = model.id
 
-    @pytest.fixture(scope='session')
-    def fixture_album_dict(
-            self,
-            fixture_album_title: str,
-            fixture_album_description: str,
-            fixture_album_tags,
-            fixture_artists_ids,
-            fixture_tracks_ids
-    ) -> dict:
-        return {
-            'album_id': None,
-            'title': fixture_album_title,
-            'description': fixture_album_description,
-            'tags': fixture_album_tags,
-            'artists_ids': fixture_artists_ids,
-            'tracks_ids': fixture_tracks_ids
-        }
-
-    """
-        Testing functions
-    """
-
-    def test_create_album(
-            self,
-            client: TestClient,
-            fixture_album_dict: AlbumTestModel
-    ):
-        response = client.post('/albums/new', json=fixture_album_dict.__dict__)
-        response_json = response.json()
-        response_status = response.status_code
-        assert response_status == status.HTTP_201_CREATED and isinstance(response_json, dict)
-        fixture_album_dict['album_id'] = response_json['id']
-
+    @pytest.mark.parametrize(  # TODO: add other statuses after auth module is done
+        'album_id,expected_status',
+        [
+            (1, status.HTTP_200_OK),
+            (100, status.HTTP_404_NOT_FOUND),
+        ]
+    )
     def test_get_album(
-            self,
-            client: TestClient,
-            fixture_album_dict: AlbumTestModel
-    ):
-        response = client.get(f"albums/{fixture_album_dict['album_id']}")
-        response_json = response.json()
-        response_status = response.status_code
-        assert response_status == status.HTTP_200_OK and isinstance(response_json, dict)
+        self,
+        client: TestClient,
+        album_id: int,
+        expected_status: int,
+    ) -> None:
+        response = client.get(url=f"albums/{album_id}")
+        assert response.status_code == expected_status
+        assert isinstance(response.json(), dict)
+        if expected_status == status.HTTP_200_OK:
+            SAlbumResponse(**response.json())
 
-    def test_view_for_album(
-            self,
-            client: TestClient,
-            fixture_album_dict: AlbumTestModel
-    ):
-        response = client.get(f"albums/{fixture_album_dict['album_id']}")
-        response_json = response.json()
-        assert response_json['views'] >= 1
-
+    @pytest.mark.parametrize(  # TODO: add other statuses after auth module is done
+        'album_id,expected_status',
+        [
+            (1, status.HTTP_200_OK),
+            (100, status.HTTP_404_NOT_FOUND),
+        ]
+    )
     def test_update_album(
-            self,
-            client: TestClient,
-            fixture_album_dict: AlbumTestModel,
-            fixture_album_title: str,
-            fixture_album_description: str,
-            fixture_picture_url: str,
-            fixture_artists_ids: list[int],
-            fixture_tracks_ids: list[int],
-            fixture_album_tags: list[str]
-    ):
-        send_body = {
-            'id': fixture_album_dict['album_id'],
-            'title': fixture_album_title,
-            'description': fixture_album_description,
-            'picture_url': fixture_picture_url,
-            'artists_ids': fixture_artists_ids,
-            'track_ids': fixture_tracks_ids,
-            'tags': fixture_album_tags
-        }
-        response = client.put(f"/albums/{fixture_album_dict['album_id']}", json=send_body)
-        response_json = response.json()
-        response_code = response.status_code
-        assert response_code == status.HTTP_201_CREATED and isinstance(response_json, dict)
+        self,
+        client: TestClient,
+        expected_status: int,
+        album: AlbumTestModel,
+        album_id: int,
+        album_title: str,
+        album_description: str,
+        album_picture_url: str,
+        album_artists_ids: list[int],
+        album_tracks_ids: list[int],
+        album_tags: list[str],
+    ) -> None:
+        request_json = SUpdateAlbumRequest(
+            id=album_id,
+            title=album_title,
+            description=album_description,
+            picture_url=album_picture_url,
+            artists_ids=album_artists_ids,
+            tracks_ids=album_tracks_ids,
+            tags=album_tags,
+        ).__dict__
+        response = client.put(url=f"/albums/{album_id}", json=request_json)
+        assert response.status_code == expected_status
+        assert isinstance(response.json(), dict)
+        if expected_status == status.HTTP_201_CREATED:
+            model = SUpdateAlbumResponse(**response.json())
+            album["id"] = model.id
 
-    def test_get_artist_albums(
-            self,
-            client: TestClient,
-            fixture_artist_id: int
-    ):
-        response = client.get(f"/albums/artist/{fixture_artist_id}")
-        response_json = response.json()
-        response_code = response.status_code
-        assert response_code == status.HTTP_200_OK and response_json['total'] >= 1
-
+    @pytest.mark.parametrize(  # TODO: add other statuses after auth module is done
+        'album_id,expected_status',
+        [
+            (1, status.HTTP_204_NO_CONTENT),
+            (100, status.HTTP_404_NOT_FOUND),
+        ]
+    )
     def test_like_album(
-            self,
-            client: TestClient,
-            fixture_album_dict: AlbumTestModel
-    ):
-        response_like = client.patch(f"/albums/{fixture_album_dict['album_id']}/like")
-        if response_like.status_code != status.HTTP_202_ACCEPTED:
-            raise False
-        response_get_by_id = client.get(f"/albums/{fixture_album_dict['album_id']}")
-        response_get_by_id_json = response_get_by_id.json()
-        assert response_get_by_id_json['likes'] >= 1
+        self,
+        client: TestClient,
+        expected_status: int,
+        album_id: int,
+    ) -> None:
+        response = client.patch(f"/albums/{album_id}/like")
+        assert response.status_code == expected_status
+        assert isinstance(response.json(), dict)
 
+    @pytest.mark.parametrize(  # TODO: add other statuses after auth module is done
+        'album_id,expected_status',
+        [
+            (1, status.HTTP_204_NO_CONTENT),
+            (100, status.HTTP_404_NOT_FOUND),
+        ]
+    )
     def test_unlike_album(
-            self,
-            client: TestClient,
-            fixture_album_dict: AlbumTestModel
-    ):
-        response_get_by_id = client.get(f"/albums/{fixture_album_dict['album_id']}")
-        likes = response_get_by_id.json()['likes']
-        if likes == 0:
-            raise Exception("like equals zero")
-        response = client.patch(f"/albums/{fixture_album_dict['album_id']}/unlike")
-        if response.status_code == status.HTTP_202_ACCEPTED:
-            assert False
-        new_likes = client.get(f"/albums/{fixture_album_dict['album_id']}").json()['likes']
-        assert likes - 1 == new_likes
+        self,
+        client: TestClient,
+        expected_status: int,
+        album_id: int,
+    ) -> None:
+        response = client.patch(f"/albums/{album_id}/unlike")
+        assert response.status_code == expected_status
+        assert isinstance(response.json(), dict)
 
     def test_get_popular_albums(
-            self,
-            client: TestClient
-    ):
-        response = client.get('/albums')
-        response_json = response.json()
-        response_code = response.status_code
-        assert response_code == status.HTTP_200_OK and response_json['total'] >= 1
+        self,
+        client: TestClient,
+    ) -> None:
+        response = client.get("/albums")
+        assert response.status_code == status.HTTP_200_OK
+        assert isinstance(response.json(), dict)
+        SPopularAlbumsResponse(**response.json())
 
+    @pytest.mark.parametrize(  # TODO: add other statuses after auth module is done
+        'album_id,expected_status',
+        [
+            (1, status.HTTP_204_NO_CONTENT),
+            (100, status.HTTP_404_NOT_FOUND),
+        ]
+    )
     def test_update_cover(
-            self,
-            client: TestClient,
-            fixture_album_dict: AlbumTestModel
-    ):
-        with open('files/avatar.jpg', 'rb') as file:
-            response = client.post(
-                f"/albums/{fixture_album_dict['album_id']}/cover",
-                files={'file': file}
-            )
-            assert response.status_code == status.HTTP_202_ACCEPTED
+        self,
+        client: TestClient,
+        expected_status: int,
+        album_id: int,
+    ) -> None:
+        request_json = SUpdateAlbumCoverRequest(
+            album_id=album_id,
+            file=UploadFile(filename='picture.png', file=SpooledTemporaryFile()),  # type: ignore[arg-type]
+        ).__dict__
+        response = client.post(
+            url=f"/albums/{album_id}/cover",
+            data=request_json,
+        )
+        assert response.status_code == expected_status
+        assert isinstance(response.json(), dict)
 
+    @pytest.mark.parametrize(  # TODO: add other statuses after auth module is done
+        'album_id,expected_status',
+        [
+            (1, status.HTTP_204_NO_CONTENT),
+            (100, status.HTTP_404_NOT_FOUND),
+        ]
+    )
     def test_delete_album(
-            self,
-            client: TestClient,
-            fixture_album_dict: AlbumTestModel
-    ):
-        client.delete(f"/albums/{fixture_album_dict['album_id']}")
-        try:
-            client.get(f"/albums/{fixture_album_dict['album_id']}")
-        except AlbumNotFoundError:
-            assert True
-        assert False
+        self,
+        client: TestClient,
+        expected_status: int,
+        album_id: int,
+    ) -> None:
+        response = client.delete(f"/albums/{album_id}")
+        assert response.status_code == expected_status
+        assert isinstance(response.json(), dict)
