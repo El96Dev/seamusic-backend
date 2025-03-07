c20332352746fb4529e621e8bbf94281140b6634
Ilya Kuznetsov <ilkztsff@gmail.com>
2025-03-08 02:28:54 +0300

    create class SMTPSessionMixin

--- src/infrastructure/email.py
+++ src/infrastructure/email.py
@@ -1,20 +1,22 @@
+from dataclasses import dataclass
 from email.mime.multipart import MIMEMultipart
 from email.mime.text import MIMEText
 
 import aiosmtplib
 
 from src.infrastructure.config import settings
-from src.infrastructure.loggers import infrastructure as logger
 
 
-async def send_email(message: str, recipient: str, subject: str) -> None:
-    sender = settings.email_address
-    password = settings.email_password
+@dataclass
+class SMTPSessionMixin:
+    @staticmethod
+    async def send_email(message: str, recipient: str, subject: str) -> None:
+        sender = settings.email_address
+        password = settings.email_password
 
-    smtp_host = settings.smtp_host
-    smtp_port = settings.smtp_port
+        smtp_host = settings.smtp_host
+        smtp_port = settings.smtp_port
 
-    try:
         server = aiosmtplib.SMTP(hostname=smtp_host, port=smtp_port)
         await server.starttls()
         await server.login(sender, password)
@@ -25,8 +27,6 @@ async def send_email(message: str, recipient: str, subject: str) -> None:
         msg["Subject"] = subject
 
         body = message
-        msg.attach(MIMEText(body, "plain"))
+        msg.attach(MIMEText(body, "plain", "utf-8"))
 
         await server.sendmail(sender, recipient, msg.as_string())
-    except Exception as ex:
-        logger.error(f"{ex}, Maybe, incorrect login or password")
