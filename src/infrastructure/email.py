b600d80ab73ffe2e82e542d435af761b28cf4e24
Ilya Kuznetsov <ilkztsff@gmail.com>
2025-03-08 23:44:00 +0300

    create Templater

--- src/infrastructure/email.py
+++ src/infrastructure/email.py
@@ -1,16 +1,31 @@
 from dataclasses import dataclass
 from email.mime.multipart import MIMEMultipart
 from email.mime.text import MIMEText
+from types import TracebackType
+from typing import Any, Self
 
 import aiosmtplib
 
 from src.infrastructure.config import settings
 
 
+class TemplateSyntaxError(SyntaxError):
+    """Syntax problem in HTML template"""
+
+
+class HTMLPage(str):
+    """HTML email template type"""
+
+
 @dataclass
 class SMTPSessionMixin:
+    """
+    SMTPSessionMixin is a mixin class for email interface implementation
+    that should be used via an asynchronous
+    """
+
     @staticmethod
-    async def send_email(message: str, recipient: str, subject: str) -> None:
+    async def send_email(message: HTMLPage, recipient: str, subject: str) -> None:
         sender = settings.email_address
         password = settings.email_password
 
@@ -30,3 +45,55 @@ class SMTPSessionMixin:
         msg.attach(MIMEText(body, "plain", "utf-8"))
 
         await server.sendmail(sender, recipient, msg.as_string())
+
+
+class Templater:
+    params: dict[str, Any]
+
+    def replace(self, string_part: str) -> str:
+        """
+        Auxiliary method for replacing parameter's placeholder with
+        parameter's value
+
+        :param string_part: part of a page after "{{"
+        :return str: same part, but parameter's value is filled
+          and ""{{"" & "}}" are removed
+        :raise IndexError: when brackets are used incorrectly
+        """
+
+        part: list[str] = string_part.split('}}')
+        param: str = part[0]
+        right: str = part[1]
+        return f"{self.params.get(param, '')}{right}"
+
+    async def parse(self, html_page: HTMLPage, **params: dict[str, Any]) -> HTMLPage:
+        """
+        Main method that fills parameters' values into template page
+
+        :param html_page: text of an HTML template
+        :param params: parameters to fill
+        :return HTMLPage: ready-to-render HTML page with filled params
+        """
+
+        self.params: dict[str, Any] = params
+
+        page: list[str] = html_page.split("{{")
+        return HTMLPage(str().join(list(map(self.replace, page))))
+
+    async def __aenter__(self) -> Self:
+        return self
+
+    async def __aexit__(
+        self,
+        exc_type: type[Exception] | None,
+        exc_val: Exception | None,
+        exc_tb: TracebackType | None,
+    ) -> None:
+        """
+        :param exc_type: exception class
+        :param exc_val: exception instance
+        :param exc_tb: full traceback
+        :raise TemplateSyntaxError: when brackets are used incorrectly
+        """
+        if exc_type == IndexError:
+            raise TemplateSyntaxError("Brackets are used incorrectly")
